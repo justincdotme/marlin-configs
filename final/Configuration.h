7f57c7cb0dbdf69f338a20721a0035af6c5c4290
Justin Christenson <info@justinc.me>
2021-03-30 07:12:06 -0700

    Tuning extruder PIDs

--- final/Configuration.h
+++ final/Configuration.h
@@ -496,13 +496,13 @@
   #if ENABLED(PID_PARAMS_PER_HOTEND)
     // Specify between 1 and HOTENDS values per array.
     // If fewer than EXTRUDER values are provided, the last element will be repeated.
-    #define DEFAULT_Kp_LIST {  22.20,  22.20 }
-    #define DEFAULT_Ki_LIST {   1.08,   1.08 }
-    #define DEFAULT_Kd_LIST { 114.00, 114.00 }
+    #define DEFAULT_Kp_LIST {  31.39,  22.20 }
+    #define DEFAULT_Ki_LIST {   4.17,   1.08 }
+    #define DEFAULT_Kd_LIST { 59.15, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  31.39
+    #define DEFAULT_Ki   4.17
+    #define DEFAULT_Kd 59.15
   #endif
 #endif // PIDTEMP
 
