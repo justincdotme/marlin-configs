1ee9de8496e3037a54245b3d27e2695ac7d86a28
Justin Christenson <info@justinc.me>
2021-04-10 12:35:27 -0700

    Tuning bed size to account for glass

--- final/Configuration.h
+++ final/Configuration.h
@@ -1123,7 +1123,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 210
+#define X_BED_SIZE 200
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
